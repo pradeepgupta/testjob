2d2a71970c4f30579c72bbcf3333c05e1e768cba
pradeepgupta <pradeepc2c@yahoo.com>
2013-04-15 15:17:50 +0530

    added entire project folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testjob::Application.config.secret_token = '992c9aecd410b0c56b045d5ff5013da4fda24b47d5f4c0a0d89e60869624348a0c8cb2f238aa1db30288f8259bd10f8141e67bd6b7995264e86cc53c09973e8b'
